14eedf9942999ae5fb7c1c26157ca2e653e049fb
JrCGO <juniosgoulart@gmail.com>
2024-02-01 21:25:54 -0300

    formação-automação-de-testes-com-cypress

--- Introdução à Qualidade de Software e Projetos Colaborativos/O que é qualidade de Software/2. Normas e padrões de qualidade.js
+++ Introdução à Qualidade de Software e Projetos Colaborativos/O que é qualidade de Software/2. Normas e padrões de qualidade.js
@@ -8,5 +8,17 @@ ISO 9004: "Diretrizes  que consideram eficácia e eficiência do sistema de gest
 ISO 9126: "Modelo de qualidade de produto de software." 
 ISO 19011: "Diretrizes sobre auditoria de sistemas de gestão da qualidade de software."
 ISO/IEC 14598: "Processo de avaliação de produtos de software na visão do desenvolvedor, adquirente e avaliador." 
+ISO/IEC/IEEE 12207:2017 | ISO/IEC/IEEE 15288:2015: "Processos de ciclo de vida do software."
 
-ISO/IEC/IEEE 12207:2017 | ISO/IEC/IEEE 15288:2015: "Processos de ciclo de vida do software"
+IEEE 1012:2016 "Verificação e validação para sisteemas, software e hardware."
+
+IEEE 730:2014 "Requerimento para planejamento, controle e execução de processos de garantia de qualidade de software."
+
+ISO/IEC/IEEE 29119:2013 "Vocabulário, processo, documentação, modelos e técnicas para teste."
+
+-> Requerimento de qualidade
+-> Modelo de Qualidade 
+-> Gerenciamento de Qualidade
+-> Metrificação de Qualidade 
+-> Avaliação de Qualidade
+
