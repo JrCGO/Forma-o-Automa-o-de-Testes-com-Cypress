ea31ca3b97ee5b4db4bf8e5092506f2b888942f7
JrCGO <juniosgoulart@gmail.com>
2024-02-01 21:18:23 -0300

    formação-automação-de-testes-com-cypress

--- Introdução à Qualidade de Software e Projetos Colaborativos/O que é qualidade de Software/2. Normas e padrões de qualidade.js
+++ Introdução à Qualidade de Software e Projetos Colaborativos/O que é qualidade de Software/2. Normas e padrões de qualidade.js
@@ -2,8 +2,11 @@
 
 Familia ISO 9000
 
-ISO 9000: Descreve fundamentos de sistemas de gestão de qualidade e suas terminologias.
-ISO 9001: Especifíca requisitos para sistemas de gestão de qualidade.
-ISO 9004: Diretrizes  que consideram eficácia e eficiência do sistema de gestão da qualidade. 
-ISO 9126: Modelo de qualidade de produto de software. 
-ISO 19011: Diretrizes sobre auditoria de sistemas de gestão da qualidade de software. 
+ISO 9000: "Descreve fundamentos de sistemas de gestão de qualidade e suas terminologias."
+ISO 9001: "Especifíca requisitos para sistemas de gestão de qualidade."
+ISO 9004: "Diretrizes  que consideram eficácia e eficiência do sistema de gestão da qualidade." 
+ISO 9126: "Modelo de qualidade de produto de software." 
+ISO 19011: "Diretrizes sobre auditoria de sistemas de gestão da qualidade de software."
+ISO/IEC 14598: "Processo de avaliação de produtos de software na visão do desenvolvedor, adquirente e avaliador." 
+
+ISO/IEC/IEEE 12207:2017 | ISO/IEC/IEEE 15288:2015: "Processos de ciclo de vida do software"
