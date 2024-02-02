ae1bf6ca1faeb8caee3c17bc7b850e539efc8786
JrCGO <juniosgoulart@gmail.com>
2024-02-01 22:41:52 -0300

    formação-automação-de-testes-com-cypress

---
+++ Introdução à Qualidade de Software e Projetos Colaborativos/O que é qualidade de Software/3. Medindo qualidade.js
@@ -0,0 +1,78 @@
+// Processo de qualdiade
+
+Stakeholders -> "Requisitos Processo de Software" <- Padrões
+
+                  "Processo de desenvolvimento"
+                      "Produto de Software"   
+
+                      Software com qualidade      
+
+//Medindo qualidade
+
+Qualidade interna: "Totalidade das características do software do ponto de vista interno"
+Métricas internas: "Podem ser aplicadas a um produto de software não executável, como especificações e código-fonte. Servem para avaliar a qualdiade do produto antes do produto ser tornar executável. São também indicadores de atributos externos."
+
+
+//Qualidade do Produto
+
+Adequação Funcional
+
+* Completude Funcional
+* Exatidão Funcional
+* Adequação Funcional
+
+
+// Performance Eficiente
+
+* Comportamento Temporal
+* Utilização de Recursos
+* Capacidade
+
+
+// Compatibilidade
+
+* Coexistência
+* Interoperabilidade
+
+
+// Usabilidade
+
+* Acessibilidade
+* Apreensibilidade
+* Estética da interface_de_Usuário.
+* Operabilidade
+* Reconhecimento Apropriado
+* Proteção contra erros do Usuário
+
+
+//Confiabilidade 
+
+* Maturidade
+* Disponibilidade
+* Tolerância a Falhas
+* Recuperabilidade
+
+
+// Segurança
+
+* Confidencialidade 
+* Integridade
+* Não Reúdio
+* Prestação de contas
+* Autenticidade
+
+
+// Manutenibilidade
+
+* Modularidade
+* Reusabilidade
+* Analisabilidade
+* Modificabilidade
+* Testabilidade
+
+
+// Portabilidade
+
+* Adaptabilidade
+* Instabilidade
+* Substituibilidade
