26e60d14d9e8fcd9766b8a96f2e4a6d9d8466e6e
JrCGO <juniosgoulart@gmail.com>
2024-02-01 21:09:09 -0300

    formação-automação-de-testes-com-cypress

---
+++ Introdução à Qualidade de Software e Projetos Colaborativos/O que é qualidade de Software/1. definindo qualdiade.js
@@ -0,0 +1,14 @@
+//Definições na literatura
+
+Aspectos importantes:
+
+// Requisitos de software são a base para medir qualidade.
+// Padrões especificados definem conjunto de critérios de desenvolvimento.
+// Existem requisitos implícitos que não são mencionados que afeta diretamente a qualidade.
+
+Visão transcedental: Qualiade é reconhecida através de experiência, mas sem uma definição ou metrificação.
+Visão do usuário: É personalizado de acordo com a necessidade do usuário.
+Visão de manufatura: Qualidade é relacionada com conformidade aos requerimentos.
+Visão de produto: Produto com boas propriedades internas metrificáveis terá boas qualidades externas.
+Visão baseada em valor: Representa o "Custo-benefício" na visão do cliente.
+
---
+++ Introdução à Qualidade de Software e Projetos Colaborativos/O que é qualidade de Software/2. Normas e padrões de qualidade.js
@@ -0,0 +1,9 @@
+// As normas para qualidade de software
+
+Familia ISO 9000
+
+ISO 9000: Descreve fundamentos de sistemas de gestão de qualidade e suas terminologias.
+ISO 9001: Especifíca requisitos para sistemas de gestão de qualidade.
+ISO 9004: Diretrizes  que consideram eficácia e eficiência do sistema de gestão da qualidade. 
+ISO 9126: Modelo de qualidade de produto de software. 
+ISO 19011: Diretrizes sobre auditoria de sistemas de gestão da qualidade de software. 
