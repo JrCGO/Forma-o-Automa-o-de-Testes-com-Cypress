24400ed6ddbe2843d6b7434c833c8b9532929aa1
JrCGO <juniosgoulart@gmail.com>
2024-02-06 19:26:25 -0300

    formação-automação-de-testes-com-cypress

---
+++ Introdução à Qualidade de Software e Projetos Colaborativos/O que é qualidade de Software/4. Processos de gerenciamento de qualidade.js
@@ -0,0 +1,43 @@
+// Processos de gerenciamento de qualidade de Software
+
+* Conjunto de todos os processos que garantem que os produtos, serviços e o ciclo de vida vão de encontro com os objetivos da qualidade e forma a alcançar a satisfação do usuário.
+
+// Atividades do gerenciamento:
+
+* Planejamento de qualidade.
+* Garantia de qualidade.
+* Conttrole de qualidade.
+* Melhoria de processos.
+
+
+// Planejamento
+
+Determinar: 
+
+-> Padrões e processos de qualidade a serem utilizados.
+-> Metas específicas de qualidade.
+-> Esforço e organização de atividades.
+
+// Garantia de qualidade
+
+-> Atividades de que definem e avaliam a adequação dos processo de software de forma a prover evidências que estabelecem confiança no produto produzido.
+
+// Controle de qualidade
+
+-> Examinação de artefatos do projeto para determinar se os padrões acordados estão sendo seguidos.
+-> Avaliação de produtos intermediários e do produto final.
+
+// Melhorias de processos
+
+-> Se preocupa com melhorias na eficiênca, efetividade e quaisquer características que tenham como meta principal a melhoria da qualidade de software. 
+
+// Outra perspectiva
+
+1. Aplicação de métodos técnicos.
+2. Realização de revisões técnicas formais.
+3. Atividades de testes de software.
+4. Aplicação de padrões.
+5. Controle de mudanças.
+6. Medição.
+7. Manutenção de registros e relatórios.
+
