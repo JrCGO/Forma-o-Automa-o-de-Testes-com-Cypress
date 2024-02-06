d708508115695936ec091205c47afc666505c638
JrCGO <juniosgoulart@gmail.com>
2024-02-06 20:09:14 -0300

    formação-automação-de-testes-com-cypress

---
+++ Introdução à Qualidade de Software e Projetos Colaborativos/2. Gerenciamento de defeitos/1. Falando em controle de qualdiade copy.js
@@ -0,0 +1,21 @@
+// Objetivos
+
+* Falando em controle de qualidade
+* Caracterizando defeitos
+* Ciclo de vida do bug
+* Ferramenta de suporte
+
+// Controle de qualidade
+
+-> Análise estática: Avaliação de documentação do software e código-fonte -> métodos formais.
+ * "Code Refiew".
+ * "Ferramentas de automação de processos de verificação de código".
+ * "Análise de histórias e modelagens".
+
+-> Análise dinâmia: Relacionado a técnicas com o código em execução.
+
+Validação "x" Verificação
+
+Verificação: Garantia que o produto está sendo contruído corretamente.
+Validação: O produto correto está sendo construído.
+
---
+++ Introdução à Qualidade de Software e Projetos Colaborativos/2. Gerenciamento de defeitos/2. Caracterizando defeitos.js
@@ -0,0 +1,28 @@
+// Rastreamento de defeitos
+
+-> Entendimento do produto e dos tipos de defeitos encontrados.
+-> Facilitar correção do processo ou do produto.
+-> Reportar status do produto.
+-> Alinhamento de revisões pelo tive de desenvolvimento.
+
+// O que é defeito?
+
+-> Gerenciamentto significa qualquer tipo de anomalia encontrada no produto
+
+-> Outras definições:
+
+    * Erro: Ação humana que produz um resultado incorreto
+
+    * Defeito: Imperfeição ou deficiência relacionada aos requerimentos e especificações do produto.
+
+    * Falha: de: sistema: Evento no qual o sistema não executa uma função sob limites especíicos.
+
+// Motivos para erros
+
+-> Pressão do tempo.
+-> Falha humana.
+-> Inexperiência e/ou falta de qualificação.
+-> Falta de comunicação.
+-> Complexidade de código, modelagem, arquitetura..
+-> Complexidade de tecnologia.
+-> Condições ambientes inesperadas.
